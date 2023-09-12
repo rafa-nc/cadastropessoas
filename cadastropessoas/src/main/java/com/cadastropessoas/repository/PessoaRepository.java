f7ddab33bab65c2c59a2a47bf9bacfba4599f25d
Rafael Costa <rafael.costa04@outlook.com>
2023-09-12 17:28:29 -0300

    Config banco de dados e repositorio.

---
+++ cadastropessoas/src/main/java/com/cadastropessoas/repository/PessoaRepository.java
@@ -0,0 +1,12 @@
+package com.cadastropessoas.repository;
+
+import com.cadastropessoas.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    List<Pessoa> findByNome(String nome);
+}
