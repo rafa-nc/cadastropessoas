4695cefcef22cffe044f3f9460260bdd36de0d27
Rafael Costa <rafael.costa04@outlook.com>
2023-09-13 14:08:51 -0300

    Import do lombok.

--- src/main/java/com/cadastropessoas/model/PessoaDto.java
+++ src/main/java/com/cadastropessoas/model/PessoaDto.java
@@ -1,11 +1,19 @@
 package com.cadastropessoas.model;
 
 
+import com.cadastropessoas.repository.PessoaRepository;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Optional;
+
+@Setter
+@Getter
 @Entity
 public class PessoaDto {
 
@@ -19,35 +27,30 @@ public class PessoaDto {
     public PessoaDto() {
     }
 
-    public String getNome() {
-        return nome;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        PessoaDto other = (PessoaDto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
 
-    public String getCpf() {
-        return cpf;
-    }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
     }
 }
